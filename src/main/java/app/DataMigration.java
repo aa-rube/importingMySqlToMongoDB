6264a6c1a4a02ae09437d72a5e912dd057ef7813
Alex Rube <alexrube93@gmail.com>
2024-05-02 18:45:24 -0300

    ihitial

---
+++ src/main/java/app/DataMigration.java
@@ -0,0 +1,57 @@
+package app;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+
+public class DataMigration {
+    public static void main(String[] args) {
+
+        String mySQLURL = "jdbc:mysql://localhost:3306/antispam";
+        String mySQLUser = "root";
+        String mySQLPassword = "30STMParadise($)_(&)";
+
+        String mongoURL = "mongodb://localhost:27017";
+
+
+        try {
+            Connection connection = DriverManager.getConnection(mySQLURL, mySQLUser, mySQLPassword);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM stop_word");
+
+            MongoDatabase mongoDatabase = MongoClients.create(mongoURL).getDatabase("antispam");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("stop_word");
+
+            List<Document> documents = new ArrayList<>();
+            while (resultSet.next()) {
+                Document doc = new Document("id", resultSet.getString("id"))
+                        .append("for_the_group_id", resultSet.getString("for_the_group_id"))
+                        .append("word", resultSet.getString("word"));
+                documents.add(doc);
+            }
+
+            if (!documents.isEmpty()) {
+                collection.insertMany(documents);
+            }
+
+            System.out.println("Данные успешно перенесены!");
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
