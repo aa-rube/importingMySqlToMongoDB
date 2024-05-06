d2d23eeb0c6180edb2c8d4bbbecf5d9773d1bb58
Alex Rube <alexrube93@gmail.com>
2024-05-06 10:02:24 -0300

    advertisers_users;

--- src/main/java/app/DataMigration.java
+++ src/main/java/app/DataMigration.java
@@ -27,15 +27,23 @@ public class DataMigration {
         try {
             Connection connection = DriverManager.getConnection(mySQLURL, mySQLUser, mySQLPassword);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM stop_word");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM advertisers_users");
 
             MongoDatabase mongoDatabase = MongoClients.create(mongoURL).getDatabase("antispam");
-            MongoCollection<Document> collection = mongoDatabase.getCollection("stop_word");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("advertisers_users;");
 
             List<Document> documents = new ArrayList<>();
             while (resultSet.next()) {
-                Document doc = new Document("word", resultSet.getString("word"))
-                        .append("for_the_group_id",resultSet.getString("for_the_group_id"));
+                Document doc = new Document(
+                        "admin_chat_id_owner", resultSet.getString("admin_chat_id_owner"))
+                        .append("admin_user_name_owner", resultSet.getString("admin_user_name_owner"))
+                        .append("days_of_permission", resultSet.getString("days_of_permission"))
+                        .append("end_permission", resultSet.getString("end_permission"))
+                        .append("permission_to_group", resultSet.getString("permission_to_group"))
+                        .append("post_count", resultSet.getString("post_count"))
+                        .append("user_name", resultSet.getString("user_name"))
+                        .append("started", resultSet.getString("started")
+                        );
                 documents.add(doc);
             }
 
